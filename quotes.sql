fbf4067c6bdb93d261db0bce4e5a01d1935b9521
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-18 12:27:06 -0500

    added quotes.sql

---
+++ quotes.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS marco_db;
+USE marco_db;
+CREATE TABLE IF NOT EXISTS quotes (
+                                      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                                      author_first_name VARCHAR(50) DEFAULT 'NONE',
+    author_last_name  VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    PRIMARY KEY (id)
+    );
