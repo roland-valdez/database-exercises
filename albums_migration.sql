7d414ff9edb6e6106bf9e20e866aa09a9dbd9b71
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-18 14:33:16 -0500

    refactored

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-   artist VARCHAR(50) NOT NULL,
+   artist VARCHAR(100) NOT NULL,
    name  VARCHAR(100) NOT NULL,
-   release_date INT NOT NULL,
-   sales FLOAT NOT NULL,
+   release_date SMALLINT UNSIGNED NOT NULL,
+   sales FLOAT(2) NOT NULL,
    genre varchar(100) NOT NULL,
    PRIMARY KEY (id)
 );
