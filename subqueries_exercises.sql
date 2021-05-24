d7da6495c6d6c1a4291e5419cfdfe310e2976471
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-24 10:59:13 -0500

    finished regular exercises

---
+++ subqueries_exercises.sql
@@ -0,0 +1,17 @@
+USE employees;
+
+SELECT first_name
+FROM employees
+WHERE hire_date IN
+(SELECT hire_date FROM employees WHERE emp_no = 101010 );
+
+SELECT DISTINCT title
+FROM titles
+WHERE emp_no IN
+(SELECT emp_no FROM employees WHERE first_name = 'Aamod' );
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (SELECT emp_no FROM dept_manager WHERE to_date > CURRENT_DATE )
+AND gender = 'f';
+
